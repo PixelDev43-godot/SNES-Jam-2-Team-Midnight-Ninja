2f74c0debc3eb5ac3637afa95a582c2452d27cc5
fallraindrop653dev <fallraindrop653@gmail.com>
2026-02-24 15:52:58 -0500

    i forget what this was

--- lunacy/TNCs and GDs/only_until_you_add_yours.gd
+++ lunacy/TNCs and GDs/only_until_you_add_yours.gd
@@ -8,13 +8,23 @@ var can_swap := true
 func _physics_process(delta):
 	rotate(0.07)
 
+	# Detect platform velocity
+	var platform_velocity := Vector2.ZERO
+	for i in get_slide_collision_count():
+		var col = get_slide_collision(i)
+		var collider = col.get_collider()
+		if collider.is_in_group("moving_platform"):
+			platform_velocity = collider.velocity
+
+	# Apply platform movement
+	global_position += platform_velocity * delta
+
+	# Your original movement
 	if Input.is_action_pressed("swap") and can_swap:
 		var forward = Vector2.UP.rotated(rotation)
 		global_position += forward * move_distance
 
-		# Rotate a full 180 degrees
 		rotation += PI
-
 		can_swap = false
 		timer.start()
 
