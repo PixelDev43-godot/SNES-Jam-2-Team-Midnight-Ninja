f6dd160ae8c54c4373b439d51ab4b55edda11750
fallraindrop653dev <fallraindrop653@gmail.com>
2026-02-24 21:21:45 -0500

    added to folder

---
+++ lunacy/TNCs and GDs/box_states.gd
@@ -0,0 +1,68 @@
+extends CharacterBody2D
+
+
+
+var one_attacking = false
+var two_attacking = true
+var rotatations_s = 0.5
+@export var attack_visuals = true
+func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("p-swap"):
+			one_attacking = !one_attacking
+			two_attacking = !two_attacking
+			attack_visuals = !attack_visuals
+	
+	if attack_visuals:
+		$BoxVisuals.modulate = Color.ORANGE
+	else:
+		$BoxVisuals.modulate = Color.BLUE
+	move_and_slide()
+	check_pit_collision()
+	check_pit_collision_two()
+				
+func _on_area_one_area_entered(area: Area2D) -> void:
+		if area.is_in_group("enemy_group"):
+			if one_attacking:
+				area.get_parent().queue_free()
+				get_parent().enemy_down()
+			else:
+				get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
+		elif area.is_in_group("hurt"):
+			get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
+		elif area.is_in_group("winner"):
+			get_tree().change_scene_to_file("res://scenes/win_screen.tscn")
+
+
+
+func _on_area_two_area_entered(area: Area2D) -> void:
+		if area.is_in_group("enemy_group"):
+			if two_attacking:
+				area.get_parent().queue_free()
+				get_parent().enemy_down()
+			else:
+				get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
+		elif area.is_in_group("hurt"):
+			get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
+		elif area.is_in_group("winner"):
+			get_tree().change_scene_to_file("res://scenes/win_screen.tscn")
+func check_pit_collision():
+	var a_one = get_node_or_null("AreaOne")
+	if a_one:
+		var overlap = a_one.get_overlapping_areas()
+		for area in overlap:
+			if area.is_in_group("pit"):
+				if one_attacking:
+					pass
+				else:
+					get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
+func check_pit_collision_two():
+	var a_two = get_node_or_null("AreaTwo")
+	if a_two:	
+		var overlap = a_two.get_overlapping_areas()
+		for area in overlap:
+			if area.is_in_group("pit"):
+				if two_attacking:
+					pass
+				else:
+					get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
